fa3e26044b692a0acfa3625bd119a5baa0aa2c25
Spencer Atkin <spenceratkin25@gmail.com>
2015-06-23 22:45:14 -0400

    Initial Commit

---
+++ Vacation-Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vacation-Tracker
+//
+//  Created by Spencer Atkin on 6/23/15.
+//  Copyright (c) 2015 Spencer Atkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
