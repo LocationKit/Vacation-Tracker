b40803359c5f37e5f1b86fd6dee4666fa670bc1f
Spencer Atkin <spenceratkin25@gmail.com>
2015-07-23 11:56:14 -0400

    Added Watch targets to podfile.

---
+++ Pods/Target Support Files/Pods-Vacation-Tracker WatchKit App/Pods-Vacation-Tracker WatchKit App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Vacation_Tracker_WatchKit_App : NSObject
+@end
+@implementation PodsDummy_Pods_Vacation_Tracker_WatchKit_App
+@end
---
+++ Pods/Target Support Files/Pods-Vacation-Tracker WatchKit App/Pods-Vacation-Tracker WatchKit App-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// LocationKit
+#define COCOAPODS_POD_AVAILABLE_LocationKit
+#define COCOAPODS_VERSION_MAJOR_LocationKit 2
+#define COCOAPODS_VERSION_MINOR_LocationKit 0
+#define COCOAPODS_VERSION_PATCH_LocationKit 8
+
