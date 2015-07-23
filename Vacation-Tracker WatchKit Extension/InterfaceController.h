049e9408ac187d243fbe544aeaccd1f08bd91fd0
Spencer Atkin <spenceratkin25@gmail.com>
2015-07-23 11:31:42 -0400

    Added watch target.

---
+++ Vacation-Tracker WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Vacation-Tracker WatchKit Extension
+//
+//  Created by Spencer Atkin on 7/23/15.
+//  Copyright (c) 2015 Spencer Atkin. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ Vacation-Tracker WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  Vacation-Tracker WatchKit Extension
+//
+//  Created by Spencer Atkin on 7/23/15.
+//  Copyright (c) 2015 Spencer Atkin. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
